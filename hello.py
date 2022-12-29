2ac6baf1d90219e50b85ec182b45f12576d3b5f5
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-12-29 15:32:46 -0800

    hello

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
+# mini program asks for name and age and converts accordingly
 print("hello there")
 print("what is your name?")
 name = input()
 print("hello" + " " + name)
+print("what is your age?")
+age = input()
+print("you will be " + str(int((age) + 1)) + " in a year")
